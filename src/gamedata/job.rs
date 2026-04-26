9c9a48ad4f51dc768d7b7fe0e1165b592a5f6fdc
BadAtGames26 <125292563+BadAtGames26@users.noreply.github.com>
2026-04-25 20:12:28 -0700

    Update job.rs
    Signed-off-by: BadAtGames26 <125292563+BadAtGames26@users.noreply.github.com>

--- src/gamedata/job.rs
+++ src/gamedata/job.rs
@@ -6,7 +6,30 @@ use crate::data::{IStructData, IStructDataMethods, StructData};
 
 #[unity2::class(namespace = "App")]
 #[parent(StructData<JobData>)]
-pub struct JobData {}
+pub struct JobData {
+    #[backing]
+    pub jid: Il2CppString,
+    #[backing]
+    pub name: Il2CppString,
+    #[backing]
+    pub aid: Il2CppString,
+    #[backing]
+    pub help: Il2CppString,
+    #[backing(name = "UnitIconID_M")]
+    pub unit_icon_id_m: Il2CppString,
+    #[backing(name = "UnitIconID_F")]
+    pub unit_icon_id_f: Il2CppString,
+    #[backing(name = "UnitIconWeaponID")]
+    pub unit_icon_weapon_id: Il2CppString,
+    #[backing]
+    pub rank: i32,
+    #[backing(name = "MaxLevel")]
+    pub max_level: u8,
+    #[backing(name = "InternalLevel")]
+    pub internal_level: i8,
+    #[backing]
+    pub sort: u16,
+}
 
 #[unity2::methods]
 impl JobData {
